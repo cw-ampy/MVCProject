a38cd19a529bbfac1977260b272691d4b70d687b
Aman Pandey <aman.pandey@aepl.com>
2022-10-11 13:57:06 +0530

    solved the images issue

--- SQLFILE_AMAN.sql
+++ SQLFILE_AMAN.sql
@@ -1,12 +1,11 @@
-create database DB_AMAN;
-use DB_AMAN;
+use internship;
 
 CREATE TABLE products_aman(	
 	id int primary key,
     Title varchar(50) NOT NULL,
     Description varchar(500) NOT NULL,
     Price float NOT NULL,
-    discountPercentage float NOT NULL,
+    Discount float NOT NULL,
     Rating float NOT NULL,
     Stock float NOT NULL,
     Brand varchar(50) NOT NULL,
@@ -14,20 +13,7 @@ CREATE TABLE products_aman(
     Thumbnail varchar(500) NOT NULL  
 );
 
-drop table productsrepo;
-
-create table images_aman(
-	imageId int primary key,
-	image1 varchar(100),
-	image2 varchar(100),
-	image3 varchar(100), 
-	image4 varchar(100),
-	image5 varchar(100),
-    image6 varchar(100)
-);
-
-
-select images.* from images inner join productsrepo on productsrepo.id = images.imageId where brand = "apple";
+CREATE table image_aman(image_link VARCHAR(255) PRIMARY KEY,id INT NOT NULL, FOREIGN KEY(id) REFERENCES products_aman(id));
 
 INSERT INTO products_aman(id,Title,Description,Price,Discount,Rating,Stock,Brand,Category,Thumbnail) VALUES (1,'iPhone 9','An apple mobile which is nothing like apple',549,12.96,4.69,94,'Apple','smartphones','https://dummyjson.com/image/i/products/1/thumbnail.jpg');
 INSERT INTO products_aman(id,Title,Description,Price,Discount,Rating,Stock,Brand,Category,Thumbnail) VALUES (2,'iPhone X','SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...',899,17.94,4.44,34,'Apple','smartphones','https://dummyjson.com/image/i/products/2/thumbnail.jpg');
@@ -61,64 +47,114 @@ INSERT INTO products_aman(id,Title,Description,Price,Discount,Rating,Stock,Brand
 INSERT INTO products_aman(id,Title,Description,Price,Discount,Rating,Stock,Brand,Category,Thumbnail) VALUES (30,'Key Holder','Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality',30,2.92,4.92,54,'Golden','home-decoration','https://dummyjson.com/image/i/products/30/thumbnail.jpg');
 
 
-select *from images;
-
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (1, 'https://dummyjson.com/image/i/products/1/1.jpg','https://dummyjson.com/image/i/products/1/2.jpg','https://dummyjson.com/image/i/products/1/3.jpg','https://dummyjson.com/image/i/products/1/4.jpg','https://dummyjson.com/image/i/products/1/thumbnail.jpg','');
-INSERT INTO  images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (2, 'https://dummyjson.com/image/i/products/2/1.jpg','https://dummyjson.com/image/i/products/2/2.jpg','https://dummyjson.com/image/i/products/2/3.jpg','https://dummyjson.com/image/i/products/2/thumbnail.jpg','','');
-INSERT INTO  images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (3,'https://dummyjson.com/image/i/products/3/1.jpg','','','','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (4, 'https://dummyjson.com/image/i/products/4/1.jpg','https://dummyjson.com/image/i/products/4/2.jpg','https://dummyjson.com/image/i/products/4/3.jpg','https://dummyjson.com/image/i/products/4/4.jpg','https://dummyjson.com/image/i/products/4/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (5, 'https://dummyjson.com/image/i/products/5/1.jpg','https://dummyjson.com/image/i/products/5/2.jpg','https://dummyjson.com/image/i/products/5/3.jpg','','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (6, 'https://dummyjson.com/image/i/products/6/1.png','https://dummyjson.com/image/i/products/6/2.jpg','https://dummyjson.com/image/i/products/6/3.png','https://dummyjson.com/image/i/products/6/4.jpg','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (7 ,'https://dummyjson.com/image/i/products/7/1.jpg','https://dummyjson.com/image/i/products/7/2.jpg','https://dummyjson.com/image/i/products/7/3.jpg','https://dummyjson.com/image/i/products/7/thumbnail.jpg','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (8, 'https://dummyjson.com/image/i/products/8/1.jpg','https://dummyjson.com/image/i/products/8/2.jpg','https://dummyjson.com/image/i/products/8/3.jpg','https://dummyjson.com/image/i/products/8/4.jpg','https://dummyjson.com/image/i/products/8/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (9, 'https://dummyjson.com/image/i/products/9/1.jpg','https://dummyjson.com/image/i/products/9/2.png','https://dummyjson.com/image/i/products/9/3.png','https://dummyjson.com/image/i/products/9/4.jpg','https://dummyjson.com/image/i/products/9/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (10, 'https://dummyjson.com/image/i/products/10/1.jpg','https://dummyjson.com/image/i/products/10/2.jpg','https://dummyjson.com/image/i/products/10/3.jpg','https://dummyjson.com/image/i/products/10/thumbnail.jpeg','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (11, 'https://dummyjson.com/image/i/products/11/1.jpg','https://dummyjson.com/image/i/products/11/2.jpg','https://dummyjson.com/image/i/products/11/3.jpg','https://dummyjson.com/image/i/products/11/thumbnail.jpg','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (12, 'https://dummyjson.com/image/i/products/12/1.jpg','https://dummyjson.com/image/i/products/12/2.jpg','https://dummyjson.com/image/i/products/12/3.png','https://dummyjson.com/image/i/products/12/4.jpg','https://dummyjson.com/image/i/products/12/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6)  VALUES (13, 'https://dummyjson.com/image/i/products/13/1.jpg','https://dummyjson.com/image/i/products/13/2.png','https://dummyjson.com/image/i/products/13/3.jpg','https://dummyjson.com/image/i/products/13/4.jpg','https://dummyjson.com/image/i/products/13/thumbnail.webp','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (14, 'https://dummyjson.com/image/i/products/14/1.jpg','https://dummyjson.com/image/i/products/14/2.jpg','https://dummyjson.com/image/i/products/14/3.jpg','https://dummyjson.com/image/i/products/14/thumbnail.jpg','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (15, 'https://dummyjson.com/image/i/products/15/1.jpg','https://dummyjson.com/image/i/products/15/2.jpg','https://dummyjson.com/image/i/products/15/3.jpg','https://dummyjson.com/image/i/products/15/4.jpg','https://dummyjson.com/image/i/products/15/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (16,'https://dummyjson.com/image/i/products/16/1.png','https://dummyjson.com/image/i/products/16/2.webp','https://dummyjson.com/image/i/products/16/3.jpg','https://dummyjson.com/image/i/products/16/4.jpg','https://dummyjson.com/image/i/products/16/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (17, 'https://dummyjson.com/image/i/products/17/1.jpg','https://dummyjson.com/image/i/products/17/2.jpg','https://dummyjson.com/image/i/products/17/3.jpg','https://dummyjson.com/image/i/products/17/thumbnail.jpg','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (18, 'https://dummyjson.com/image/i/products/18/1.jpg','https://dummyjson.com/image/i/products/18/2.jpg','https://dummyjson.com/image/i/products/18/3.jpg','https://dummyjson.com/image/i/products/18/4.jpg','https://dummyjson.com/image/i/products/18/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (19, 'https://dummyjson.com/image/i/products/19/1.jpg','https://dummyjson.com/image/i/products/19/2.jpg','https://dummyjson.com/image/i/products/19/3.png','https://dummyjson.com/image/i/products/19/thumbnail.jpg','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (20,'https://dummyjson.com/image/i/products/20/1.jpg','https://dummyjson.com/image/i/products/20/2.jpg','https://dummyjson.com/image/i/products/20/3.jpg','https://dummyjson.com/image/i/products/20/4.jpg','https://dummyjson.com/image/i/products/20/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (21, 'https://dummyjson.com/image/i/products/21/1.png','https://dummyjson.com/image/i/products/21/2.jpg','https://dummyjson.com/image/i/products/21/3.jpg','','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (22, 'https://dummyjson.com/image/i/products/22/1.jpg','https://dummyjson.com/image/i/products/22/2.jpg','https://dummyjson.com/image/i/products/22/3.jpg','','','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (23, 'https://dummyjson.com/image/i/products/23/1.jpg','https://dummyjson.com/image/i/products/23/2.jpg','https://dummyjson.com/image/i/products/23/3.jpg','https://dummyjson.com/image/i/products/23/4.jpg','https://dummyjson.com/image/i/products/23/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (24,'https://dummyjson.com/image/i/products/24/1.jpg','https://dummyjson.com/image/i/products/24/2.jpg','https://dummyjson.com/image/i/products/24/3.jpg','https://dummyjson.com/image/i/products/24/4.jpg','https://dummyjson.com/image/i/products/24/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (25, 'https://dummyjson.com/image/i/products/25/1.png','https://dummyjson.com/image/i/products/25/2.jpg','https://dummyjson.com/image/i/products/25/3.png','https://dummyjson.com/image/i/products/25/4.jpg','https://dummyjson.com/image/i/products/25/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (26, 'https://dummyjson.com/image/i/products/26/1.jpg','https://dummyjson.com/image/i/products/26/2.jpg','https://dummyjson.com/image/i/products/26/3.jpg','https://dummyjson.com/image/i/products/26/4.jpg','https://dummyjson.com/image/i/products/26/5.jpg','https://dummyjson.com/image/i/products/26/thumbnail.jpg');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (27, 'https://dummyjson.com/image/i/products/27/1.jpg','https://dummyjson.com/image/i/products/27/2.jpg','https://dummyjson.com/image/i/products/27/3.jpg','https://dummyjson.com/image/i/products/27/4.jpg','https://dummyjson.com/image/i/products/27/thumbnail.webp','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (28, 'https://dummyjson.com/image/i/products/28/1.jpg','https://dummyjson.com/image/i/products/28/2.jpg','https://dummyjson.com/image/i/products/28/3.png','https://dummyjson.com/image/i/products/28/4.jpg','https://dummyjson.com/image/i/products/28/thumbnail.jpg','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (29, 'https://dummyjson.com/image/i/products/29/1.jpg','https://dummyjson.com/image/i/products/29/2.jpg','https://dummyjson.com/image/i/products/29/3.webp','https://dummyjson.com/image/i/products/29/4.webp','https://dummyjson.com/image/i/products/29/thumbnail.webp','');
-INSERT INTO images_aman(imageId,image1,image2,image3,image4,image5, image6) VALUES (30, 'https://dummyjson.com/image/i/products/30/1.jpg','https://dummyjson.com/image/i/products/30/2.jpg','https://dummyjson.com/image/i/products/30/3.jpg','https://dummyjson.com/image/i/products/30/thumbnail.jpg','','');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO image_aman(image_link, id) values
+	       ('https://dummyjson.com/image/i/products/1/1.jpg',1),
+	('https://dummyjson.com/image/i/products/1/2.jpg',1),
+    ('https://dummyjson.com/image/i/products/1/3.jpg',1),
+    ('https://dummyjson.com/image/i/products/1/4.jpg',1),
+    ('https://dummyjson.com/image/i/products/2/1.jpg',2),
+    ('https://dummyjson.com/image/i/products/2/2.jpg',2),
+    ('https://dummyjson.com/image/i/products/2/3.jpg',2),
+    ('https://dummyjson.com/image/i/products/3/1.jpg',3),
+    ('https://dummyjson.com/image/i/products/4/1.jpg',4),
+    ('https://dummyjson.com/image/i/products/4/2.jpg',4),
+    ('https://dummyjson.com/image/i/products/4/3.jpg',4),
+    ('https://dummyjson.com/image/i/products/4/4.jpg',4),
+    ('https://dummyjson.com/image/i/products/5/1.jpg',5),
+    ('https://dummyjson.com/image/i/products/5/2.jpg',5),
+    ('https://dummyjson.com/image/i/products/5/3.jpg',5),
+    ('https://dummyjson.com/image/i/products/6/1.png',6),
+    ('https://dummyjson.com/image/i/products/6/2.jpg',6),
+    ('https://dummyjson.com/image/i/products/6/3.png',6),
+    ('https://dummyjson.com/image/i/products/6/4.jpg',6),
+    ('https://dummyjson.com/image/i/products/7/1.jpg',7),
+    ('https://dummyjson.com/image/i/products/7/2.jpg',7),
+    ('https://dummyjson.com/image/i/products/7/3.jpg',7),
+    ('https://dummyjson.com/image/i/products/8/1.jpg',8),
+    ('https://dummyjson.com/image/i/products/8/2.jpg',8),
+    ('https://dummyjson.com/image/i/products/8/3.jpg',8),
+    ('https://dummyjson.com/image/i/products/8/4.jpg',8),
+    ('https://dummyjson.com/image/i/products/9/1.jpg',9),
+    ('https://dummyjson.com/image/i/products/9/2.png',9),
+    ('https://dummyjson.com/image/i/products/9/3.png',9),
+    ('https://dummyjson.com/image/i/products/9/4.jpg',9),
+    ('https://dummyjson.com/image/i/products/10/1.jpg',10),
+    ('https://dummyjson.com/image/i/products/10/2.jpg',10),
+    ('https://dummyjson.com/image/i/products/10/3.jpg',10),
+    ('https://dummyjson.com/image/i/products/11/1.jpg',11),
+    ('https://dummyjson.com/image/i/products/11/2.jpg',11),
+    ('https://dummyjson.com/image/i/products/11/3.jpg',11),
+    ('https://dummyjson.com/image/i/products/12/1.jpg',12),
+    ('https://dummyjson.com/image/i/products/12/2.jpg',12),
+    ('https://dummyjson.com/image/i/products/12/3.png',12),
+    ('https://dummyjson.com/image/i/products/12/4.jpg',12),
+    ('https://dummyjson.com/image/i/products/13/1.jpg',13),
+    ('https://dummyjson.com/image/i/products/13/2.png',13),
+    ('https://dummyjson.com/image/i/products/13/3.jpg',13),
+    ('https://dummyjson.com/image/i/products/13/4.jpg',13),
+    ('https://dummyjson.com/image/i/products/14/1.jpg',14),
+    ('https://dummyjson.com/image/i/products/14/2.jpg',14),
+    ('https://dummyjson.com/image/i/products/14/3.jpg',14),
+    ('https://dummyjson.com/image/i/products/15/1.jpg',15),
+    ('https://dummyjson.com/image/i/products/15/2.jpg',15),
+    ('https://dummyjson.com/image/i/products/15/3.jpg',15),
+    ('https://dummyjson.com/image/i/products/15/4.jpg',15),
+    ('https://dummyjson.com/image/i/products/16/1.jpg',16),
+    ('https://dummyjson.com/image/i/products/16/2.webp',16),
+    ('https://dummyjson.com/image/i/products/16/3.jpg',16),
+    ('https://dummyjson.com/image/i/products/16/4.jpg',16),
+    ('https://dummyjson.com/image/i/products/17/1.jpg',17),
+    ('https://dummyjson.com/image/i/products/17/2.jpg',17),
+    ('https://dummyjson.com/image/i/products/17/3.jpg',17),
+    ('https://dummyjson.com/image/i/products/18/1.jpg',18),
+    ('https://dummyjson.com/image/i/products/18/2.jpg',18),
+    ('https://dummyjson.com/image/i/products/18/3.jpg',18),
+    ('https://dummyjson.com/image/i/products/18/4.jpg',18),
+    ('https://dummyjson.com/image/i/products/19/1.jpg',19),
+    ('https://dummyjson.com/image/i/products/19/2.jpg',19),
+    ('https://dummyjson.com/image/i/products/19/3.png',19),
+    ('https://dummyjson.com/image/i/products/20/1.jpg',20),
+    ('https://dummyjson.com/image/i/products/20/2.jpg',20),
+    ('https://dummyjson.com/image/i/products/20/3.jpg',20),
+    ('https://dummyjson.com/image/i/products/20/4.jpg',20),
+    ('https://dummyjson.com/image/i/products/21/1.png',21),
+    ('https://dummyjson.com/image/i/products/21/2.jpg',21),
+    ('https://dummyjson.com/image/i/products/21/3.jpg',21),
+    ('https://dummyjson.com/image/i/products/22/1.jpg',22),
+    ('https://dummyjson.com/image/i/products/22/2.jpg',22),
+    ('https://dummyjson.com/image/i/products/22/3.jpg',22),
+    ('https://dummyjson.com/image/i/products/23/1.jpg',23),
+    ('https://dummyjson.com/image/i/products/23/2.jpg',23),
+    ('https://dummyjson.com/image/i/products/23/3.jpg',23),
+    ('https://dummyjson.com/image/i/products/23/4.jpg',23),
+    ('https://dummyjson.com/image/i/products/24/1.jpg',24),
+    ('https://dummyjson.com/image/i/products/24/2.jpg',24),
+    ('https://dummyjson.com/image/i/products/24/3.jpg',24),
+    ('https://dummyjson.com/image/i/products/24/4.jpg',24),
+    ('https://dummyjson.com/image/i/products/25/1.png',25),
+    ('https://dummyjson.com/image/i/products/25/2.jpg',25),
+    ('https://dummyjson.com/image/i/products/25/3.png',25),
+    ('https://dummyjson.com/image/i/products/25/4.jpg',25),
+    ('https://dummyjson.com/image/i/products/26/1.jpg',26),
+    ('https://dummyjson.com/image/i/products/26/2.jpg',26),
+    ('https://dummyjson.com/image/i/products/26/3.jpg',26),
+    ('https://dummyjson.com/image/i/products/26/4.jpg',26),
+    ('https://dummyjson.com/image/i/products/26/5.jpg',26),
+    ('https://dummyjson.com/image/i/products/27/1.jpg',27),
+    ('https://dummyjson.com/image/i/products/27/2.jpg',27),
+    ('https://dummyjson.com/image/i/products/27/3.jpg',27),
+    ('https://dummyjson.com/image/i/products/27/4.jpg',27),
+    ('https://dummyjson.com/image/i/products/28/1.jpg',28),
+    ('https://dummyjson.com/image/i/products/28/2.jpg',28),
+    ('https://dummyjson.com/image/i/products/28/3.jpg',28),
+    ('https://dummyjson.com/image/i/products/28/4.jpg',28),
+    ('https://dummyjson.com/image/i/products/29/1.jpg',29),
+    ('https://dummyjson.com/image/i/products/29/2.jpg',29),
+    ('https://dummyjson.com/image/i/products/29/3.webp',29),
+    ('https://dummyjson.com/image/i/products/29/4.webp',29),
+    ('https://dummyjson.com/image/i/products/30/1.jpg',30),
+    ('https://dummyjson.com/image/i/products/30/2.jpg',30),
+    ('https://dummyjson.com/image/i/products/30/3.jpg',30);
